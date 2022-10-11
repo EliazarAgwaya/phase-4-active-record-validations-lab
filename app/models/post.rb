6df3cb8ba0df49954fe88d22f91faf558c88a3e3
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-10-11 14:35:27 +0300

    commit tree 67f68dc07a87d774ef1864fc621c4baea49c41d2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: (["Fiction", "Non-Fiction"])}
+    validate :click_bait? 
+
+    CLICKBAIT_PATTERNS = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+    def click_bait? 
+        if CLICKBAIT_PATTERNS.none? {|x| x.match title}
+            errors.add(:title, "Title must contain clickbait!")
+        end
+    end
 end
